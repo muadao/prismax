36793e81fe5c318de823b00d23a0aea8346302fd
Yozora <yozoraworks@users.noreply.github.com>
2024-10-01 15:46:58 +0800

    .

--- app/page.js
+++ app/page.js
@@ -13,6 +13,15 @@ export default function RootLayout() {
       <Intro />
       <ModelPage />
       <Third />
+      <div style={{ height: '40pt', width: '80%', margin: '0pt 10%',
+        display: 'flex', flexDirection: 'row', justifyContent: 'space-between',
+        alignItems: 'center'
+       }}>
+        <img src='/logo.svg' alt='footer' style={{ height: '40pt' }} />
+        <p
+        style={{ fontSize: '14pt', color: 'rgba(255, 255, 255, 0.7)' }}
+        >A Base Layer for Real-World Multimodal GenAI Apps</p>
+      </div>
     </>
 
   )
--- components/third/Third.js
+++ components/third/Third.js
@@ -1,9 +1,70 @@
 'use client'
 
 import React from "react";
+import Icon from "../images/index";
+
+const apps = [
+    { url: "https://discord.gg/xh8vR6JVzM", name: "Discord" },
+    { url: "https://x.com/PrismaXai", name: "Twitter" },
+    { url: "https://t.me/PrismaX_News", name: "Telegram" },
+];
 
 export default function Third() {
     return (
-        <div id='three3' style={{ width: '100%', height: '100vh' }}></div>
+        <div style={{
+            width: '100%', height: '100vh', display: 'flex',
+            justifyContent: 'center', alignItems: 'center',
+            flexDirection: 'row',
+        }}>
+            <div style={{
+                width: '50%', textAlign: 'center',
+                display: 'flex', flexDirection: 'column', justifyContent: 'space-around'
+            }}>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                    <h1 style={{ fontSize: '50pt', maxWidth: '400pt' }}
+                    >Let's Stary Connected</h1>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '40pt' }}>
+                    {apps.map((item, index) => {
+                        return (
+                            <div
+                                onClick={() => item.url && window.open(item.url, "_blank")}
+                                key={`app_${index}`}
+                                className="sociallink"
+                                style={{
+                                    display: 'flex', flexDirection: 'row', justifyContent: 'center',
+                                    gap: '6pt',
+                                    fontSize: '14pt',
+                                    height: '16pt',
+                                    cursor: 'pointer',
+                                    color: 'rgba(255, 255, 255, 0.7)',
+                                }}
+                            >
+                                <Icon
+                                    name={item.name}
+                                    height={20}
+                                    width={20}
+                                />
+                                {item.name}
+                                <div>
+                                    <Icon
+                                        name={"IconArrow"}
+                                        className="sociallinkarrow"
+                                    />
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div style={{ width: '50%', textAlign: 'center' }}>
+                <img src='/stayConnected.png' alt='stay connected'
+                    style={{ width: '100%', height: '100%' }}
+                />
+            </div>
+        </div>
     )
 }
